187c67bd0153b93cbfe4152634615a88b0b42a85
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-09-22 08:53:21 +0100

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -23,13 +23,13 @@ const Navbar = () => {
 
   return (
     <>
-      <section className=''>
+      <section className='dark:h-screen'>
         <div className='flex'>
           <div className='text-purple-500 lg:ms-[500px] ms-5 dark:text-red-100 mt-5'>
             <BiBookAlt />
           </div>
 
-          <select className='lg:ms-[230px] ms-48 mt-5 text-purple-500 dark:text-red-500' onChange={(e) => setFont(e.target.value)}>
+          <select className='lg:ms-[230px] ms-40 mt-5 text-purple-500 dark:text-red-500' onChange={(e) => setFont(e.target.value)}>
             <option value= "Serif">Serif</option>
             <option value= "Montserrat">Montserrat</option>
             <option value="Poppins">Poppins</option>
